91dc1059d52f14d6e8690c42d74da531229874ec
Jake Soenneker <jake@soenneker.com>
2024-12-19 15:54:18 -0600

    Tests

---
+++ test/Soenneker.Utils.File.Types.Tests/FileTypeUtilTests.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Soenneker.Utils.File.Types.Abstract;
+using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+
+namespace Soenneker.Utils.File.Types.Tests;
+
+[Collection("Collection")]
+public class FileTypeUtilTests : FixturedUnitTest
+{
+    private readonly IFileTypeUtil _util;
+
+    public FileTypeUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+        _util = Resolve<IFileTypeUtil>(true);
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
