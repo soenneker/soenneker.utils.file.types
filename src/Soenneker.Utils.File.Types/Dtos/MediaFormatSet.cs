c702dc915b4cfde326839a53b9763efaa54cfcfe
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:42 -0500

    xml commenting

--- src/Soenneker.Utils.File.Types/Dtos/MediaFormatSet.cs
+++ src/Soenneker.Utils.File.Types/Dtos/MediaFormatSet.cs
@@ -2,9 +2,18 @@
 
 namespace Soenneker.Utils.File.Types.Dtos;
 
+/// <summary>
+/// Represents the media format set.
+/// </summary>
 public sealed class MediaFormatSet
 {
+    /// <summary>
+    /// Gets or sets video codecs.
+    /// </summary>
     public List<string> VideoCodecs { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets audio codecs.
+    /// </summary>
     public List<string> AudioCodecs { get; set; } = null!;
 }
